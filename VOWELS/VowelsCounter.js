1ec902a4ec56e36b4115b61d5907e341df655501
Sergey Makarov <sgmakgg@gmail.com>
2022-12-04 13:19:26 +0300

    FD2HW7

--- VOWELS/VowelsCounter.js
+++ VOWELS/VowelsCounter.js
@@ -10,20 +10,16 @@
 
 function vowelsCounter(text){
     var russianVowels = {'а': 0, 'у': 0, 'о': 0, 'ы':0, 'и':0, 'э':0, 'я':0, 'ю':0, 'ё':0, 'е':0};
-    var summary = 0;
+    var counter = 0;
     var countedString = text.toLowerCase();
 
     for (let i = 0; i < countedString.length; i++) {
         if(countedString[i] in russianVowels){
-            var temp = countedString[i];
-            russianVowels[temp]++;
+            counter++;
         }
     }
-    
-    for (const key in russianVowels) {
-        summary += russianVowels[key];
-    }
-    return summary;
+
+    return counter;
 }
 
 

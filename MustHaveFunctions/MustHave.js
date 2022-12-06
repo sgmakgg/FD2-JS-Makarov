656271726e1b6fffda5fd4c564f550d49d8e41e9
Sergey Makarov <sgmakgg@gmail.com>
2022-12-06 16:30:07 +0300

    round&random

--- MustHaveFunctions/MustHave.js
+++ MustHaveFunctions/MustHave.js
@@ -19,3 +19,13 @@ function getNumWord(num,word1,word2,word5) {
     return word5;
 }
 
+// округление до произвольного модуля
+function roundMod(n,m) {
+    return Math.round(n/m)*m;
+}
+
+// получение целого случайного числа в заданном диапазоне
+function randomDiap(n,m) {
+    return Math.floor(Math.random()*(m-n+1))+n;
+}
+

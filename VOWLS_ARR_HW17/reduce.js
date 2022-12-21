bd6621d563015c784892d0db408c1ddcf6aa3c2f
Sergey Makarov <sgmakgg@gmail.com>
2022-12-21 17:20:15 +0300

    HW17

--- VOWLS_ARR_HW17/reduce.js
+++ VOWLS_ARR_HW17/reduce.js
@@ -1,18 +1,9 @@
 function vowelsCounterReduce(text){
     let russianVowels = {'а': 0, 'у': 0, 'о': 0, 'ы': 0, 'и': 0, 'э': 0, 'я': 0, 'ю': 0, 'ё': 0, 'е': 0};
-    let countedString = text.toLowerCase();
+    let arr = text.toLowerCase().split('');
 
-    let arr = countedString.split('');
-    let result = arr.reduce((russianVowels, vowel) => {
-            russianVowels[vowel]++;
-        return russianVowels;},
-        russianVowels);
-
-    let cnt = 0;
-    for (const key in result) {
-        if(!isNaN(result[key]))
-            cnt += result[key];
-    }
-
-    return cnt;
+    return arr.reduce((cnt, vowel) => {
+        if (vowel in russianVowels)
+            cnt ++;
+        return cnt;}, 0);
 }

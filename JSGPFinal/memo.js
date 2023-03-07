1ab8e56d09b71d4603c5c6a5b8c2ab21d1975221
Sergey Makarov <sgmakgg@gmail.com>
2023-03-07 11:43:19 +0300

    Final project (audio, refactoring)

--- JSGPFinal/memo.js
+++ JSGPFinal/memo.js
@@ -4,53 +4,55 @@ function memoGame() {
             localStorage.setItem(key, value);
         }
 
-        function get(key) {
+        function getValue(key) {
             return localStorage.getItem(key);
         }
 
-        function increase(key) {
-            setLocalStorage(key, parseInt(get(key)) + 1);
+        function increaseScore(key) {
+            setLocalStorage(key, parseInt(getValue(key)) + 1);
         }
 
-        function decrease(key) {
-            setLocalStorage(key, parseInt(get(key)) - 1);
+        function decreaseScore(key) {
+            setLocalStorage(key, parseInt(getValue(key)) - 1);
         }
 
-        let toTime = function (nr) {
-            if (nr === '-:-') return nr;
+        let toTime = function (value) {
+            if (value === '-:-')
+                return value;
             else {
-                let n = ' ' + nr / 1000 + ' ';
-                return n.substr(0, n.length - 1) + 's';
+                let time = ' ' + value / 1000 + ' ';
+                return time.substr(0, time.length - 1) + 's';
             }
         };
 
         function updateStats() {
             document.getElementById('stats').innerHTML = '' +
                 '<div class="padded">' +
-                        '<h2>Figures: <span>' + '<b>' + get('flip_won') + '</b><i>Won</i>' +
-                        '<b>' + get('flip_lost') + '</b><i>Lost</i>' +
-                        '<b>' + get('flip_abandoned') + '</b><i>Abandoned</i></span>' +
+                        '<h2>Figures: <span>' + '<b>' + getValue('flip_won') + '</b><i>Won</i>' +
+                        '<b>' + getValue('flip_lost') + '</b><i>Lost</i>' +
+                        '<b>' + getValue('flip_abandoned') + '</b><i>Abandoned</i></span>' +
                         '</h2>' +
                     '<ul>' +
                         '<li>' +
-                        '<b>Best Casual:</b> <span>' + toTime(get('flip_casual')) + '</span>' +
+                        '<b>Best Casual:</b> <span>' + toTime(getValue('flip_casual')) + '</span>' +
                         '</li>' +
                         '<li>' +
-                        '<b>Best Medium:</b> <span>' + toTime(get('flip_medium')) + '</span>' +
+                        '<b>Best Medium:</b> <span>' + toTime(getValue('flip_medium')) + '</span>' +
                         '</li>' +
                         '<li>' +
-                        '<b>Best Hard:</b> <span>' + toTime(get('flip_hard')) + '</span>' +
+                        '<b>Best Hard:</b> <span>' + toTime(getValue('flip_hard')) + '</span>' +
                         '</li>' +
                     '</ul>' +
                     '<ul>' +
                         '<li>' +
-                        '<b>Total Flips:</b> <span>' + parseInt((parseInt(get('flip_matched')) + parseInt(get('flip_wrong'))) * 2) + '</span>' +
+                        '<b>Total Flips:</b> <span>' + parseInt((parseInt(getValue('flip_matched'))
+                                                    + parseInt(getValue('flip_wrong'))) * 2) + '</span>' +
                         '</li>' +
                         '<li>' +
-                        '<b>Matched Flips:</b> <span>' + get('flip_matched') + '</span>' +
+                        '<b>Matched Flips:</b> <span>' + getValue('flip_matched') + '</span>' +
                         '</li>' +
                         '<li>' +
-                        '<b>Wrong Flips:</b> <span>' + get('flip_wrong') + '</span>' +
+                        '<b>Wrong Flips:</b> <span>' + getValue('flip_wrong') + '</span>' +
                         '</li>' +
                     '</ul>' +
                 '</div>';
@@ -81,39 +83,40 @@ function memoGame() {
             document.querySelector('.c4').innerHTML = text.substring(3, 4);
 
             // If won game
-            if (text === 'nice') {
-                increase('flip_won');
-                decrease('flip_abandoned');
+            if (text === 'cool') {
+                increaseScore('flip_won');
+                decreaseScore('flip_abandoned');
             }
 
             // If lost game
-            else if (text === 'fail') {
-                increase('flip_lost');
-                decrease('flip_abandoned');
+            else if (text === 'oops') {
+                increaseScore('flip_lost');
+                decreaseScore('flip_abandoned');
             }
 
             updateStats();
         }
 
-        /* LOAD GAME ACTIONS */
-
+        // Game load
         // Init localStorage
-        if (!get('flip_won') && !get('flip_lost') && !get('flip_abandoned')) {
+        if (!getValue('flip_won') && !getValue('flip_lost') && !getValue('flip_abandoned')) {
             //Overall Game stats
             setLocalStorage('flip_won', 0);
             setLocalStorage('flip_lost', 0);
             setLocalStorage('flip_abandoned', 0);
+
             //Best times
             setLocalStorage('flip_casual', '-:-');
             setLocalStorage('flip_medium', '-:-');
             setLocalStorage('flip_hard', '-:-');
+
             //Cards stats
             setLocalStorage('flip_matched', 0);
             setLocalStorage('flip_wrong', 0);
         }
 
         // Fill stats
-        if (get('flip_won') > 0 || get('flip_lost') > 0 || get('flip_abandoned') > 0) {
+        if (getValue('flip_won') > 0 || getValue('flip_lost') > 0 || getValue('flip_abandoned') > 0) {
             updateStats();
         }
 
@@ -121,10 +124,10 @@ function memoGame() {
         document.querySelectorAll('.logo .card').forEach(
             (element) => {
                             if(!element.classList.contains('twist'))
-                                element.addEventListener('click', switcher);
+                                element.addEventListener('click', toggleCards);
                             });
 
-        function switcher(eo){
+        function toggleCards(eo){
             eo = eo || window.event;
 
             let currentElement = eo.currentTarget;
@@ -148,8 +151,19 @@ function memoGame() {
         }
 
     function startCurrentGame (eo) {
-            eo = eo || window.event;
-        increase('flip_abandoned');
+        eo = eo || window.event;
+
+        let currentGameAudio = new Audio('sounds/simple-funny-background-music-intro.mp3');
+        soundInit(currentGameAudio);
+        playSound(currentGameAudio);
+
+        let pairCardSound = new Audio("sounds/funny-boing-sound-effect.mp3");
+        soundInit(pairCardSound);
+
+        let winGameSound = new Audio("sounds/funny-blowing-trumpet-sound-effect.mp3");
+        soundInit(winGameSound);
+
+        increaseScore('flip_abandoned');
 
         document.querySelector('.info').style.display = 'none';
 
@@ -160,7 +174,7 @@ function memoGame() {
         // Set game timer and difficulty
         if (level === 8) {
             difficulty = 'casual';
-            timer *= level * 4;
+            timer *= level * 6;
         } else if (level === 18) {
             difficulty = 'medium';
             timer *= level * 5;
@@ -223,6 +237,7 @@ function memoGame() {
                     let thisCards = gameElement.querySelectorAll('.active .b[data-f=' + data + ']');
 
                     if (thisCards.length > 1) {
+                        pairCardSound.play();
                         thisCards = document.querySelectorAll('div.card.active');
                         for (const thisCard of thisCards) {
                             let result = true;
@@ -238,16 +253,18 @@ function memoGame() {
                             }
                         }
                         check = 0;
-                        increase('flip_matched');
+                        increaseScore('flip_matched');
 
                         // Win game
                         if (!document.querySelector('#g .card')) {
                             let time = Date.now() - startGame;
-                            if (get('flip_' + difficulty) === '-:-' || get('flip_' + difficulty) > time) {
-                                setLocalStorage('flip_' + difficulty, time); // increase best score
+                            if (getValue('flip_' + difficulty) === '-:-' || getValue('flip_' + difficulty) > time) {
+                                // increase best score
+                                setLocalStorage('flip_' + difficulty, time);
                             }
-
-                            startScreen('nice');
+                            startScreen('cool');
+                            currentGameAudio.pause();
+                            winGameSound.play();
                         }
                     }
                     else {
@@ -255,29 +272,32 @@ function memoGame() {
                         for (const thisCard of thisCards) {
                             thisCard.classList.remove('active');
                         }
-
-                        increase('flip_wrong');
+                        increaseScore('flip_wrong');
                     }
                 }, 401);
             }
-            }
+        }
 
         let elem = document.createElement('i');
         elem.className += 'timer';
         elem.style.animation = 'timer ' + timer + 'ms linear';
 
         elem.addEventListener('animationend', function timerFunction() {
-            startScreen('fail');
+            startScreen('oops');
+            currentGameAudio.pause();
             elem.removeEventListener('animationend', timerFunction);
         });
 
         document.getElementById('g').prepend(elem);
+
 
         //pause & emergency exit
         window.addEventListener('keyup', function (eo) {
 
             if (eo.key === 'p') {
                 if (parseInt(document.body.getAttribute('data-paused')) === 1) { //was paused, now resume
+                    currentGameAudio.play();
+
                     document.body.setAttribute('data-paused', '0');
                     document.querySelector('.timer').style.animationPlayState = 'running';
 
@@ -285,6 +305,8 @@ function memoGame() {
                     elem.removeChild(document.querySelector('div.pause'));
                 }
                 else {
+                    currentGameAudio.pause();
+                    
                     document.body.setAttribute('data-paused', '1');
                     document.querySelector('.timer').style.animationPlayState = 'paused';
 
@@ -298,5 +320,17 @@ function memoGame() {
                 window.close();
             }
         });
+
+        function soundInit(sound) {
+            sound.play(); // запускаем звук
+            sound.pause(); // и сразу останавливаем
+        }
+
+        function playSound(sound) {
+            sound.currentTime=0; // в секундах
+            sound.play();
+            sound.loop = true;
+            sound.volume = 0.2;
+        }
     }
 }

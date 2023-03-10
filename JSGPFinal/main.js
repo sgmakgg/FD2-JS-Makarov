3bf67478907f70abcc4515cf8f0a7b421719188f
Sergey Makarov <sgmakgg@gmail.com>
2023-03-10 03:45:57 +0300

    modules&lite-server

--- JSGPFinal/main.js
+++ JSGPFinal/main.js
@@ -1,68 +1,12 @@
-import {soundInit, playSound} from './soundsFunctions.js';
+import {soundInit, playSound} from './sounds.js';
+import {setLocalStorage, getValue, increaseScore, decreaseScore, updateStats} from './statistic.js';
 
 document.addEventListener('DOMContentLoaded', memoGame);
 function memoGame() {
-        if ( "ontouchstart" in window )
+        if ("ontouchstart" in window)
             document.querySelector('#touches').innerHTML = 'Touches are supporting';
         else
             document.querySelector('#touches').innerHTML = "Touches aren't supporting";
-        function setLocalStorage(key, value) {
-            localStorage.setItem(key, value);
-        }
-
-        function getValue(key) {
-            return localStorage.getItem(key);
-        }
-
-        function increaseScore(key) {
-            setLocalStorage(key, parseInt(getValue(key)) + 1);
-        }
-
-        function decreaseScore(key) {
-            setLocalStorage(key, parseInt(getValue(key)) - 1);
-        }
-
-        let toTime = function (value) {
-            if (value === '-:-')
-                return value;
-            else {
-                let time = ' ' + value / 1000 + ' ';
-                return time.substr(0, time.length - 1) + 's';
-            }
-        };
-
-        function updateStats() {
-            document.getElementById('stats').innerHTML = '' +
-                '<div class="padded">' +
-                        '<h2>Figures: <span>' + '<b>' + getValue('flip_won') + '</b><i>Won</i>' +
-                        '<b>' + getValue('flip_lost') + '</b><i>Lost</i>' +
-                        '<b>' + getValue('flip_abandoned') + '</b><i>Abandoned</i></span>' +
-                        '</h2>' +
-                    '<ul>' +
-                        '<li>' +
-                        '<b>Best Casual:</b> <span>' + toTime(getValue('flip_casual')) + '</span>' +
-                        '</li>' +
-                        '<li>' +
-                        '<b>Best Medium:</b> <span>' + toTime(getValue('flip_medium')) + '</span>' +
-                        '</li>' +
-                        '<li>' +
-                        '<b>Best Hard:</b> <span>' + toTime(getValue('flip_hard')) + '</span>' +
-                        '</li>' +
-                    '</ul>' +
-                    '<ul>' +
-                        '<li>' +
-                        '<b>Total Flips:</b> <span>' + parseInt((parseInt(getValue('flip_matched'))
-                                                    + parseInt(getValue('flip_wrong'))) * 2) + '</span>' +
-                        '</li>' +
-                        '<li>' +
-                        '<b>Matched Flips:</b> <span>' + getValue('flip_matched') + '</span>' +
-                        '</li>' +
-                        '<li>' +
-                        '<b>Wrong Flips:</b> <span>' + getValue('flip_wrong') + '</span>' +
-                        '</li>' +
-                    '</ul>' +
-                '</div>';
-        }
 
         function shuffle(array) {
             let currentIndex = array.length, temporaryValue, randomIndex;
@@ -347,18 +291,6 @@ function memoGame() {
                 document.body.appendChild(elem);
             }
         }
-        
-        // function soundInit(sound) {
-        //     sound.play();
-        //     sound.pause();
-        // }
-        //
-        // function playSound(sound) {
-        //     sound.currentTime = 0;
-        //     sound.play();
-        //     sound.loop = true;
-        //     sound.volume = 0.2;
-        // }
     }
 }
 

be864680bd36607389ecb0d595fe3e99d4e95b0f
Sergey Makarov <sgmakgg@gmail.com>
2023-03-12 13:52:52 +0300

    add

--- MustHaveFunctions/MustHave.js
+++ MustHaveFunctions/MustHave.js
@@ -167,3 +167,81 @@ function scrollToElem(elem,scrollMode) {
             break;
     }
 }
+
+// возвращает размер открытого документа
+function getDocumentSize() {
+    var totalHeight=(document.body.scrollHeight > document.body.offsetHeight)?document.body.scrollHeight:document.body.offsetHeight;
+    var totalWidth=(document.body.scrollWidth > document.body.offsetWidth)?document.body.scrollWidth:document.body.offsetWidth;
+    return {width:totalWidth, height:totalHeight};
+}
+
+// возвращает размер клиентской области окна
+function getWindowClientSize() {
+    var uaB=navigator.userAgent.toLowerCase();
+    var isOperaB = (uaB.indexOf('opera')  > -1);
+    var isIEB=(!isOperaB && uaB.indexOf('msie') > -1);
+
+    var clientWidth=((document.compatMode||isIEB)&&!isOperaB)?
+        (document.compatMode=='CSS1Compat')?
+            document.documentElement.clientWidth:
+            document.body.clientWidth:
+        (document.parentWindow||document.defaultView).innerWidth;
+
+    var clientHeight=((document.compatMode||isIEB)&&!isOperaB)?
+        (document.compatMode=='CSS1Compat')?
+            document.documentElement.clientHeight:
+            document.body.clientHeight:
+        (document.parentWindow||document.defaultView).innerHeight;
+
+    return {width:clientWidth, height:clientHeight};
+}
+
+// возвращает, насколько проскроллировано окно браузера
+function getWindowScrollPos() {
+    if ( 'pageXOffset' in window )
+        return { scrollx: window.pageXOffset, scrolly: window.pageYOffset };
+    if ( document.documentElement && ('scrollLeft' in document.documentElement) )
+        return { scrollx: document.documentElement.scrollLeft, scrolly: document.documentElement.scrollTop };
+    if ( document.body && ('scrollLeft' in document.body) )
+        return { scrollx: document.body.scrollLeft, scrolly: document.body.scrollTop };
+    return { scrollx: 0, scrolly: 0 };
+}
+
+// скроллирует окно к указанному элементу
+function scrollToElem(elem,scrollMode) {
+    // scrollMode:
+    // 0 - сделать видимым с минимальным скроллингом
+    // 1 - верх объекта - к верху экрана
+    // 2 - объект на середину экрана
+    // 3 - низ объекта - к низу экрана
+    // 4 - верх объекта - почти к верху экрана
+
+    var elemPos=getElementPos(elem);
+    switch ( scrollMode ) {
+        case 0:
+            var scrollPos=getWindowScrollPos();
+            if ( scrollPos.scrolly>elemPos.top )
+                window.scrollTo(0,elemPos.top);
+            else {
+                var clientSize=getWindowClientSize();
+                if ( elemPos.top+elem.offsetHeight>scrollPos.scrolly+clientSize.height )
+                    window.scrollTo(0,elemPos.top+elem.offsetHeight-clientSize.height);
+            }
+            break;
+        case 1:
+            window.scrollTo(0,elemPos.top);
+            break;
+        case 2:
+            var clientSize=getWindowClientSize();
+            window.scrollTo(0,elemPos.top+elem.offsetHeight/2-clientSize.height/2);
+            break;
+        case 3:
+            var clientSize=getWindowClientSize();
+            window.scrollTo(0,elemPos.top+elem.offsetHeight-clientSize.height);
+            break;
+        case 4:
+            var clientSize=getWindowClientSize();
+            window.scrollTo(0,elemPos.top-clientSize.height/5);
+            break;
+    }
+}
